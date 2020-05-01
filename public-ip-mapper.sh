19f62127e14a77fa1746029c797a392aeb6b8734
ivstiv <ivan21039@gmail.com>
2020-05-01 18:12:17 +0100

    Converting from tabs to spaces :D

--- public-ip-mapper.sh
+++ public-ip-mapper.sh
@@ -6,83 +6,83 @@
 # if you want to log the output: bash public-ip-mapper.sh 2>&1 | tee file.log
 
 removeRule() {
-	echo "Removing rules from -t nat POSTROUTING: $1"
-	rules=$(/sbin/iptables -t nat -S POSTROUTING -w | grep $1 | cut -f 1 -d ' ' --complement)
-	if [[ -z "$rules" ]]; then
-		echo "Couldn't find any references."
-	else
-		while IFS= read -r rule; do
-			/sbin/iptables -t nat -D $rule -w
-    		echo "Removed: $rule"
-		done <<< "$rules"
-	fi
+    echo "Removing rules from -t nat POSTROUTING: $1"
+    rules=$(/sbin/iptables -t nat -S POSTROUTING -w | grep $1 | cut -f 1 -d ' ' --complement)
+    if [[ -z "$rules" ]]; then
+        echo "Couldn't find any references."
+    else
+        while IFS= read -r rule; do
+            /sbin/iptables -t nat -D $rule -w
+            echo "Removed: $rule"
+        done <<< "$rules"
+    fi
 }
 
 listRule() {
-	rules=$(/sbin/iptables -t nat -S POSTROUTING -w | grep $1 | cut -f 1 -d ' ' --complement)
-	if [[ -z "$rules" ]]; then
-		echo "Couldn't find any references."
-	else
-		while IFS= read -r rule; do
-			formattedRule=$(echo "$rule" | awk '{ $7 = substr($7, 11, 36); print $7" :: "$3" -> "$11; }')
-    		echo "$formattedRule"
-		done <<< "$rules"
-	fi
+    rules=$(/sbin/iptables -t nat -S POSTROUTING -w | grep $1 | cut -f 1 -d ' ' --complement)
+    if [[ -z "$rules" ]]; then
+        echo "Couldn't find any references."
+    else
+        while IFS= read -r rule; do
+            formattedRule=$(echo "$rule" | awk '{ $7 = substr($7, 11, 36); print $7" :: "$3" -> "$11; }')
+            echo "$formattedRule"
+        done <<< "$rules"
+    fi
 }
 
 if [[ $1 == "--remove" ]]; then
-	# quick validation
-	[[ -z $2 ]] && echo -e "Specify server id or \"all\"" && exit
-	[[ $2 == "all" ]] && removeRule ip-mapper || removeRule $2
-	exit
+    # quick validation
+    [[ -z $2 ]] && echo -e "Specify server id or \"all\"" && exit
+    [[ $2 == "all" ]] && removeRule ip-mapper || removeRule $2
+    exit
 
 elif [[ $1 == "--list" ]]; then
-	# quick validation
-	[[ -z $2 ]] && echo -e "Specify server id or \"all\"" && exit
-	[[ $2 == "all" ]] && listRule ip-mapper || listRule $2
-	exit
+    # quick validation
+    [[ -z $2 ]] && echo -e "Specify server id or \"all\"" && exit
+    [[ $2 == "all" ]] && listRule ip-mapper || listRule $2
+    exit
 fi
 
 echo "Listening for docker events..."
 docker events --filter type=container --format '{{.Status}} {{.Actor.Attributes.name}}' | while read event
 
 do
-	status=$(echo $event | awk '{print $1}')
-	if [[ $status == 'start' ]]; then
-		server_id=$(echo $event | awk '{print $2}')
-		local_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $server_id)
-		public_ip=$(docker exec $server_id printenv SERVER_IP)
-		
-		echo "========================================="
-		echo $(date)
-		echo -e "Status=$status\nServer_ID=$server_id\nLocal_IP=$local_ip\nPublic_IP=$public_ip"
-		echo "Action: Adding to NAT"
-		echo "Trying to remove old rules just in case..."
-		removeRule $server_id
-		echo "Adding the new rule..."
-		
-		if [[ -z $public_ip ]]; then
-			echo "Missing environmental variable: SERVER_IP"
-			echo "Cannot be added to iptables!"
-		else
-			# add new rule 
-			/sbin/iptables -t nat -I POSTROUTING -s $local_ip -j SNAT --to $public_ip -m comment --comment ip-mapper-$server_id -w
-			echo "Finished."
-		fi
-		echo "========================================="
-	elif [[ $status == 'die' ]]; then
-		server_id=$(echo $event | awk '{print $2}')
-		
-		echo "========================================="
-		echo $(date)
-		echo -e "Status=$status\nServer_ID=$server_id"
-		echo "Action: Removing from NAT by Server ID"
-		removeRule $server_id
-		echo "Finished."
-		echo "========================================="
-	fi
-	
-	# You can configure all events from here: https://docs.docker.com/engine/reference/commandline/events/
+    status=$(echo $event | awk '{print $1}')
+    if [[ $status == 'start' ]]; then
+        server_id=$(echo $event | awk '{print $2}')
+        local_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $server_id)
+        public_ip=$(docker exec $server_id printenv SERVER_IP)
+        
+        echo "========================================="
+        echo $(date)
+        echo -e "Status=$status\nServer_ID=$server_id\nLocal_IP=$local_ip\nPublic_IP=$public_ip"
+        echo "Action: Adding to NAT"
+        echo "Trying to remove old rules just in case..."
+        removeRule $server_id
+        echo "Adding the new rule..."
+        
+        if [[ -z $public_ip ]]; then
+            echo "Missing environmental variable: SERVER_IP"
+            echo "Cannot be added to iptables!"
+        else
+            # add new rule 
+            /sbin/iptables -t nat -I POSTROUTING -s $local_ip -j SNAT --to $public_ip -m comment --comment ip-mapper-$server_id -w
+            echo "Finished."
+        fi
+        echo "========================================="
+    elif [[ $status == 'die' ]]; then
+        server_id=$(echo $event | awk '{print $2}')
+        
+        echo "========================================="
+        echo $(date)
+        echo -e "Status=$status\nServer_ID=$server_id"
+        echo "Action: Removing from NAT by Server ID"
+        removeRule $server_id
+        echo "Finished."
+        echo "========================================="
+    fi
+    
+    # You can configure all events from here: https://docs.docker.com/engine/reference/commandline/events/
     
 done
 
